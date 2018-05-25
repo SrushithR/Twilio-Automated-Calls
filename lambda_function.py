9fb70f7f2e96e3b7e1f0a600a4b33cd4f6a381e5
Srushith <srushith.codeops@gmail.com>
2018-05-25 16:37:59 +0530

    added inline comments

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,15 @@
 """
 
 def lambda_handler(event, context):
+    # for debugging, first print event contents
     print 'input to lambda {}'.format(event)
+
+    # twilio expets an XML content for generating the voice. This is written in something strange called TwiML
+    # Ignore all the blah blah in the angular brackets and the actual voice message is "Hi! ....".
+    # The voice is spoken by Alice - <Say voice = "alice">
+    # The actual voice content is within the <Response> tags
     xml_content = """<?xml version="1.0" encoding="utf-8"?><Response><Say voice = "alice">Hi! Hope you are having a great time hacking code in the meetup</Say></Response>"""
-    return {'body' : xml_content}
+    
+    # frustratingly, lambda can only send JSON content so we are wrapping it in the dummy JSON variable
+    dummy = {'body' : xml_content} 
+    return dummy
