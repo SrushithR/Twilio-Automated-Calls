621aadf513617c3e1a87fa4b512cb3167f2484e5
Srushith <srushith.codeops@gmail.com>
2018-05-27 14:37:11 +0530

    updated the step by step instructions file to create a new role

--- make_phone_call.py
+++ make_phone_call.py
@@ -2,17 +2,17 @@
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC5*******************811d8e4d445a"
-auth_token = "ef65********************aeb3"
+account_sid = "AC5ed8a0e4df********1d8e4d445a"
+auth_token = "ef651e0abc6*************ff93d214caeb3"
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
     # the verified number to which you wanna call
-    to="+918686519259",
+    to="+919916994022",
     # the number that you just purchased on Twilio
-    from_="+18034087781",
+    from_="+18123018775",
     # the invoke URL from API Gateway
-    url="https://lskcr****.execute-api.us-east-1.amazonaws.com/dev/twilio"
+    url="https://ls******7.execute-api.us-east-1.amazonaws.com/dev/twilio"
 )
 # An SID is generated for every call. It is useful for debugging
 print(call.sid)
